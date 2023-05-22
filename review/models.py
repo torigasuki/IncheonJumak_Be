3b211ee661b2a11a36492729f06dffec55d66200
gangyuji <gangyoyul9@gmail.com>
2023-05-22 22:32:12 +0900

    ✨  리뷰 작성, 조회, 수정, 삭제 기능 구현

--- review/models.py
+++ review/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+# from user.models import User 
 
 # Create your models here.
+class Review(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=40)
+    content = models.TextField()
+    image = models.ImageField(blank=True, upload_to='%Y/%m/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return str(self.title)
---
+++ review/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from review.models import Review
+
+class ReviewSerializer(serializers.ModelSerializer):
+    # user = serializers.SerializerMethodField()
+    
+    # def get_user(self, obj):
+    #     return obj.user.email
+    
+    class Meta:
+        model = Review
+        fields = '__all__'
+        
+class ReviewCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ("title", "image", "content")
+        
+class ReviewListSerializer(serializers.ModelSerializer):
+    # user = serializers.SerializerMethodField()
+    
+    # def get_user(self, obj):
+    #     return obj.user.email
+    
+    class Meta:
+        model = Review
+        fields = ("pk", "title", "image", "updated_at") 
