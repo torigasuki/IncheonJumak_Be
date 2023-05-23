27e4edb6db44491c56d1897b999f24ac423b1065
yunity <130277553+gangyuji@users.noreply.github.com>
2023-05-23 17:24:30 +0900

    🐛  리뷰 작성, 조회, 수정 삭제 기능 에러 수정 (#8)

--- review/models.py
+++ review/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-# from user.models import User 
+from user.models import User 
 
 # Create your models here.
 class Review(models.Model):
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=40)
     content = models.TextField()
-    image = models.ImageField(blank=True, upload_to='%Y/%m/')
+    image = models.ImageField(blank=True, upload_to='%Y/%m/%d')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
--- review/views.py
+++ review/views.py
@@ -14,10 +14,10 @@ class ReviewView(APIView):
         serializer = ReviewListSerializer(reviews, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     def post(self, request):
-        # print(request.user)
+        print(request.user)
         serializer = ReviewCreateSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -29,22 +29,24 @@ class ReviewDetailView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
     def put(self, request, review_id):
         review = get_object_or_404(Review, id=review_id)
-        # if request.user == review.user:
-        serializer = ReviewCreateSerializer(review, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+        print(request.user)
+        print(review.user)
+        if request.user == review.user:
+            serializer = ReviewCreateSerializer(review, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    # else:
-    #     return Response("권한이 없습니다.", status=status.HTTP_403_FORBIDDEN)
+            return Response("권한이 없습니다.", status=status.HTTP_403_FORBIDDEN)
     def delete(self, request, review_id):
         review = get_object_or_404(Review, id=review_id)
-        # if request.user == review.user:
-        #     review.delete()
-        #     return Response("삭제되었습니다.", status=status.HTTP_204_NO_CONTENT)
-        # else:
-        #     return Response("권한이 없습니다.", status=status.HTTP_403_FORBIDDEN)
+        if request.user == review.user:
+            review.delete()
+            return Response("삭제되었습니다.", status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response("권한이 없습니다.", status=status.HTTP_403_FORBIDDEN)
     
 class CommentView(APIView):
     def get(self, request):
