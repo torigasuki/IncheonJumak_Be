5f9d5403d3829021a7ec32441ebaf2aef2a0c7ce
diane073 <127281732+diane073@users.noreply.github.com>
2023-05-23 14:24:31 +0900

    ✨ Follow, Bookmark 기능 (#5)
    * ✨ follow 기능, following/follower 보기 기능
    
    * ✨ Bookmark 기능
    
    * Update models.py

--- user/urls.py
+++ user/urls.py
@@ -11,5 +11,15 @@ urlpatterns = [
     path('verify/', views.VerificationEmailView.as_view(), name='verify_email'),
     path('login/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    #follow 기능 url
+    path("<int:user_id>/follow/", views.FollowView.as_view(), name="follow_user"),
+    path("<int:user_id>/followings/", views.FollowingView.as_view(), name="followings"),
+    path("<int:user_id>/followers/", views.FollowerView.as_view(), name="followers"),
+
+    #bookmark 기능 url
+    path("<int:user_id>/bookmark/", views.BookMarkView.as_view(), name="bookmark_view"),
+    path("<int:user_id>/bookmark_list/",views.BookMarkListView.as_view(),name="bookmark_list_view",),
+
     path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
