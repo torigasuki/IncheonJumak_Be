e0b4e8280ee716f98abbbe5146bef682af9aac49
Seung Ho <127724780+torigasuki@users.noreply.github.com>
2023-05-23 16:16:49 +0900

    ✨ 프로필 보기 및 이미지 업로드 기능 구현 (#7)
    * :ambulance: Settings에 SIMPLE_JWT 설정 추가
    
    * :sparkles: 프로필 보기 및 이미지 업로드 기능 구현

--- user/urls.py
+++ user/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     path('verify/', views.VerificationEmailView.as_view(), name='verify_email'),
     path('login/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
     #follow 기능 url
     path("<int:user_id>/follow/", views.FollowView.as_view(), name="follow_user"),
     path("<int:user_id>/followings/", views.FollowingView.as_view(), name="followings"),
@@ -20,6 +19,5 @@ urlpatterns = [
     #bookmark 기능 url
     path("<int:user_id>/bookmark/", views.BookMarkView.as_view(), name="bookmark_view"),
     path("<int:user_id>/bookmark_list/",views.BookMarkListView.as_view(),name="bookmark_list_view",),
-
     path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
