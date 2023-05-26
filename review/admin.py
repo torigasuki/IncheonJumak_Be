2fdce65f803a0304e6a6a8f718277230084f62f6
Seung Ho <127724780+torigasuki@users.noreply.github.com>
2023-05-26 18:34:02 +0900

    * :art: Review 앱 구조 개선 중 (#24)
    * :art: Review 앱 구조 개선 중
    
    각 페이지 별 리뷰 세분화 목적

--- review/admin.py
+++ review/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from review.models import Review
+from review.models import Alcohol_Review, Brewery_Review,Event_Review
 
 # Register your models here.
-admin.site.register(Review)
+admin.site.register(Alcohol_Review)
+admin.site.register(Brewery_Review)
+admin.site.register(Event_Review)
